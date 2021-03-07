915e2191022b7e0b36ffc9a707b0ca774dd56e9a
mahmoudadel54 <mahmoodmadel@gmail.com>
2021-03-07 11:57:02 +0200

    create store -reducer -action- make a dispatch the action

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { createStore } from 'redux';
 function App() {
+  //the initial state 
+  //it is JS Object
+  const initState = {
+    posts:[],
+    todos:[]
+  }
+
+  //first create a reducer as store must contain one reducer at least
+  //reducer is a js function that returns some of data from the store
+  //params of reducer should be state (by default = initialized state of the store) ,action
+  const firstReducer = (state = initState, action)=>{              
+    console.log(action, state);
+    // return []
+  }
+  //create a store and pass all reducers to it
+  const store = createStore(firstReducer);      //once store is created, reducer will fire auomatically even if there is no dispatch
+
+
+  //let's create an action
+  //it is JS Object, must contain 'type' and can contain any payload data
+  const todoAction = {
+    type:'ADD_TODO',
+    todo:'Take a Redux Course'
+  }
+
+  //dispatch the action from store
+  store.dispatch(todoAction);
+  //what will happen : 
+  //1- go to the store with todoAction -> pass action to store's reducer -> fire the reducer
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Redux App</h1>
     </div>
   );
 }
